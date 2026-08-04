8f3552630351b0168107fe2e58b158308b82b223
fe2-web2081-summer26-main <taquan2508@gmail.com>
2026-08-04 15:36:31 +0700

    Cập nhật code JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("javascript");
+let name = "taquan";
+const school = "FPT Polytechnic";
+
+let isAdmin = true;
+
+console.log(name);
+alert("Xin chào JavaScript");
+
+const name = "taquan"
+const age = 20;
+
+document.getElementById("title").innerHTML = "Xin chào JavaScript";
