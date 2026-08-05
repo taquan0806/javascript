3a918c25494a0f6563ac822e2d115f2178319100
fe2-web2081-summer26-main <taquan2508@gmail.com>
2026-08-05 15:04:23 +0700

    gh

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-console.log("javascript");
-let name = "taquan";
-const school = "FPT Polytechnic";
+// console.log("javascript");
+// let name = "taquan";
+// const school = "FPT Polytechnic";
 
-let isAdmin = true;
+// let isAdmin = true;
 
-console.log(name);
-// alert("Xin chào JavaScript");
+// console.log(name);
+// // alert("Xin chào JavaScript");
 
-name = "quanta"
-const age = 20;
+// name = "quanta"
+// const age = 20;
 
-// document.getElementById("title").innerHTML = "Xin chào JavaScript";
+// // document.getElementById("title").innerHTML = "Xin chào JavaScript";
 
-console.log(name);
-console.log(age==20);
+// console.log(name);
+// console.log(age==20);
+
+const hoTen = "Tạ Quân";
+const tuoi = 20;
+
+console.log("Họ tên:", hoTen);
+console.log("Tuổi:", tuoi);
+
+document.getElementById("info").innerHTML =
+    `Họ tên: ${hoTen} <br> Tuổi: ${tuoi}`;
