17ec0dfce1f71f8307059b652023fa63c542101e
fe2-web2081-summer26-main <taquan2508@gmail.com>
2026-08-05 15:07:24 +0700

    i

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ console.log("Họ tên:", hoTen);
 console.log("Tuổi:", tuoi);
 
 document.getElementById("info").innerHTML =
-    `Họ tên: ${hoTen} <br> Tuổi: ${tuoi}`;
+   `Họ tên: ${hoTen} <br> Tuổi: ${tuoi}`;
